2666b76e352a2d1ec1daef30389efcde4e71c02e
カグチ <59391215+KagChi@users.noreply.github.com>
2021-02-22 14:11:45 +0700

    Update Shoukaku.js

--- src/Shoukaku.js
+++ src/Shoukaku.js
@@ -19,8 +19,6 @@ class Shoukaku extends EventEmitter {
      */
     constructor(client, nodes, options) {
         super();
-        if (!version.startsWith('12'))
-            throw new ShoukakuError('Shoukaku will only work in Discord.JS v12. Versions below Discord.JS v12 is not supported.');
         if (!nodes || !nodes.length) 
             throw new ShoukakuError('No nodes supplied');
         /**
